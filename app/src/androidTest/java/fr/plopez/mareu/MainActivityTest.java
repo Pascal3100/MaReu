8ab38efa797307bd14ce61edde26f6c114ceb005
palopez <lopez.pascal.pro@gmail.com>
2021-06-02 10:26:46 +0200

    add jump_to_filter_fragment_and_do_filter_test test in MainActivityTest

--- app/src/androidTest/java/fr/plopez/mareu/MainActivityTest.java
+++ app/src/androidTest/java/fr/plopez/mareu/MainActivityTest.java
@@ -1,6 +1,7 @@
 package fr.plopez.mareu;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -21,26 +22,27 @@ import fr.plopez.mareu.view.main.MainActivity;
 
 import static androidx.test.espresso.Espresso.onView;
 
-import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.pressBack;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-    @Rule
-    public ActivityScenarioRule mActivityRule = new ActivityScenarioRule(MainActivity.class);
-
     private static final List<Meeting> meetingList = FakeMeetingsGen.generateFakeMeetingList(RoomsRepository.getRoomsRepositoryInstance());
     private static final int ITEMS_COUNT = meetingList.size();
     private static final int SELECTED_MEETING_POSITION = 1;
+    private static final String FILTER_DIALOG_MATCH_TEXT = "1 " + ApplicationProvider.getApplicationContext().getString(R.string.filter_text_message_singular);
 
+    @Rule
+    public ActivityScenarioRule mActivityRule = new ActivityScenarioRule(MainActivity.class);
 
     @Before
     public void setUp() {
@@ -97,18 +99,40 @@ public class MainActivityTest {
     }
 
     @Test
+    public void jump_to_filter_fragment_and_do_filter_test() {
+        // click on the filter action button to load the filter fragment
+        onView(withId(R.id.sort_meetings)).perform(click());
+
+        // click on the flower room item filter
+        onView(allOf(withId(R.id.room_filter), isDisplayed()))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(
+                        2, click())
+                );
+
+        // verify the text is updated the right way
+        onView(withId(R.id.filter_text)).check(matches(withText(containsString(FILTER_DIALOG_MATCH_TEXT))));
+
+        // click on close button
+        onView(withId(R.id.closeFilterDialogButton)).perform(click());
+
+        // verify that the list contain only the filtered meeting
+        onView(allOf(withId(R.id.main_activity_fragment_meeting_recycler_view), isDisplayed()))
+                .check(RecyclerViewItemCountAssertion.withItemCount(1));
+    }
+
+    @Test
     public void main_activity_delete_meeting_test() {
         // verify we have the right number of meetings
-        onView(allOf(withId(R.id.meeting_container), isDisplayed()))
+        onView(allOf(withId(R.id.main_activity_fragment_meeting_recycler_view), isDisplayed()))
                 .check(RecyclerViewItemCountAssertion.withItemCount(ITEMS_COUNT));
 
         // click on the second meeting holder delete button
-        onView(allOf(withId(R.id.meeting_container), isDisplayed()))
+        onView(allOf(withId(R.id.main_activity_fragment_meeting_recycler_view), isDisplayed()))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(
                         SELECTED_MEETING_POSITION, new DeleteViewAction()));
 
         // verify that the holder is deleted
-        onView(allOf(withId(R.id.meeting_container), isDisplayed()))
+        onView(allOf(withId(R.id.main_activity_fragment_meeting_recycler_view), isDisplayed()))
                 .check(RecyclerViewItemCountAssertion.withItemCount(ITEMS_COUNT-1));
     }
 }
