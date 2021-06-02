f99fd78d2b9848111bfcac04d0b838397b923baf
palopez <lopez.pascal.pro@gmail.com>
2021-06-02 13:52:12 +0200

    code refacto and warning fixes in RoomsRepository

--- app/src/test/java/fr/plopez/mareu/RoomsRepositoryTest.java
+++ app/src/test/java/fr/plopez/mareu/RoomsRepositoryTest.java
@@ -1,6 +1,5 @@
 package fr.plopez.mareu;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -9,21 +8,24 @@ import fr.plopez.mareu.data.RoomsRepository;
 import fr.plopez.mareu.data.model.Room;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RoomsRepositoryTest {
 
+    // constants
+
+    // rules
+
+    // mocks
+
+    // class variables
     private final RoomsRepository roomsRepository = RoomsRepository.getRoomsRepositoryInstance();
-    private final String CORRECT_ROOM_INPUT = "Mushroom";
-    private final String INCORRECT_ROOM_INPUT = "INCORRECT_ROOM_INPUT";
-
-    @Before
-    public void setup(){
-
-    }
 
     @Test
     public void get_room_object_by_name_nominal_case(){
+        String CORRECT_ROOM_INPUT = "Mushroom";
+
         Room result = roomsRepository.getRoomByName(CORRECT_ROOM_INPUT);
 
         assertEquals(result.getName(), CORRECT_ROOM_INPUT.toLowerCase());
@@ -31,8 +33,10 @@ public class RoomsRepositoryTest {
 
     @Test
     public void get_room_object_by_name_wrong_name_case(){
+        String INCORRECT_ROOM_INPUT = "INCORRECT_ROOM_INPUT";
+
         Room result = roomsRepository.getRoomByName(INCORRECT_ROOM_INPUT);
 
-        assertEquals(result, null);
+        assertNull(result);
     }
 }
